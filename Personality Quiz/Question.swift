fa39d61f5ab43cdad79caf34a4d856092233f167
student <student@student-3.local>
2018-09-21 13:42:03 +0300

    Added questions and answers

--- Personality Quiz/Question.swift
+++ Personality Quiz/Question.swift
@@ -25,4 +25,51 @@ struct Answer {
 
 enum AnimalType: Character {
     case dog = "🐶", cat = "🐱", rabbit = "🐰", turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "Вам нравиться быть с друзьямы, вы общительны"
+        case .cat:
+            return "Вы себе на уме, любите гулять в одиночестве"
+        case .rabbit:
+            return "Вы обожаете активный отдых и семью"
+        case .turtle:
+            return "Вы мудры и осторожны.  Медленный выигрывает."
+        }
+    }
 }
+
+var questions: [Question] = [
+    
+    Question(
+        text: "Какая пища вам нравится?",
+        type: .single,
+        answers: [
+            Answer(text: "Мясо", type: .dog),
+            Answer(text: "Рыба", type: .cat),
+            Answer(text: "Морковка", type: .carrot),
+            Answer(text: "Кукуруза", type: .turtle)
+        ]),
+    
+    Question(
+        text: "Что вы любите делать?",
+        type: .multiple,
+        answers: [
+            Answer(text: "Есть", type: .dog),
+            Answer(text: "Спать", type: .cat),
+            Answer(text: "Обниматься", type: .carrot),
+            Answer(text: "Плавать", type: .turtle)
+        ]),
+    
+    
+    Question(
+        text: "Насколько вы любите поездки на машине?",
+        type: .multiple,
+        answers: [
+            Answer(text: "Ненавижу", type: .cat),
+            Answer(text: "Нервничаю", type: .carrot),
+            Answer(text: "Не замечаю", type: .turtle),
+            Answer(text: "Обожаю", type: .dog)
+        ])
+]
